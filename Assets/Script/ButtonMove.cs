47102e25ed3c3f3e7b346c58174befe6a34491a4
mymacbook <vokar@shernet.sheridancollege.ca>
2025-02-18 05:49:36 -0500

    it works…. but only for one click…? need explain later since I though I did it right ?

--- Assets/Script/ButtonMove.cs
+++ Assets/Script/ButtonMove.cs
@@ -5,42 +5,62 @@ using UnityEngine;
 public class ButtonMove : MonoBehaviour
 {
     public float speed = 2f;
+    public Transform newspaper; 
 
-    //public bool left = false; in case
-    //public bool right = false;
+    private bool left = false; 
+    private bool right = false;
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += speed * Time.deltaTime;
+        //Vector3 pos = transform.position;
+        //pos.x += speed * Time.deltaTime;
 
-        Vector2 screenPos = Camera.main.WorldToScreenPoint (pos);
-        if (screenPos.x<0)
-        {
-            Vector3 fixedPos = new Vector3(0, 0, 0);
-            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
-            speed = speed * -1;
-        }
-        if (screenPos.x > Screen.width)
-        {
-            Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
-            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
-            speed = speed * -1;
-        }
-        transform.position = pos;
-    }
-  
-    public void GoLeft (float s)
-    {
-        speed = -s;
+        //Vector2 screenPos = Camera.main.WorldToScreenPoint (pos);
+        //if (screenPos.x<0)
+        //{
+        //    Vector3 fixedPos = new Vector3(0, 0, 0);
+        //    pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+        //    speed = speed * -1;
+        //}
+        //if (screenPos.x > Screen.width)
+        //{
+        //    Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
+        //    pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+        //    speed = speed * -1;
+        //}
+        //transform.position = pos;
+        // finding this one that talked about transform https://discussions.unity.com/t/setting-a-variable-as-public-transform/607629/3
+
+        if (left)
+        
+            newspaper.position += Vector3.left * speed * Time.deltaTime;
+
+        if (right)
+
+            newspaper.position += Vector3.right * speed * Time.deltaTime;
+   
     }
 
-    public void GoRight(float s)
+  public void moveLeft()
     {
-        speed = s;
+        left = true;
     }
-    public void stop()
+
+    public void moveRight()
     {
-        speed = 0;
+        right = true;
     }
+    //public void GoLeft (float s)
+    //{
+    //    speed = -s;
+    //}
+
+    //public void GoRight(float s)
+    //{
+    //    speed = s;
+    //}
+    //public void stop()
+    //{
+    //    speed = 0;
+    //}
 }
