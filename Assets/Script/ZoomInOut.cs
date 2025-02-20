2a7bb1f80d71bb5870712022b05359198b76cf6f
mymacbook <vokar@shernet.sheridancollege.ca>
2025-02-20 16:05:08 -0500

    Fixing around

--- Assets/Script/ZoomInOut.cs
+++ Assets/Script/ZoomInOut.cs
@@ -17,7 +17,7 @@ public class ZoomInOut : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (zoomSlider = null)// zoom s
+        if (zoomSlider = null)// zoom slider 
         {
             t = zoomSlider.value;
         }
